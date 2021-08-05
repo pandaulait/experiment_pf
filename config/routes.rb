b73bc2487c10238267e908a39152f17320620792
pandaulait <show.hey.panda@gmail.com>
2021-08-05 13:38:19 +0000

    [Start]

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+    sessions: 'public/devise/sessions',
+    passwords: 'public/devise/passwords',
+    registrations: 'public/devise/registrations',
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
